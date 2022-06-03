6ce10f5bd9c6a633bb679384265055ce08cd9dbe
Anirban Karmakar <anirban.k@mantralabs.tech>
2022-06-03 00:29:25 -0700

    First push

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categories', function () {
+    return "All available categories to choose tutors from displayed here.";
+});
